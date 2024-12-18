14a4accf80125a0e98fd727b3b0bd56ab23c9aa7
linuxfight <kiko13024@gmail.com>
2024-12-19 00:17:19 +0300

    add docker, logger and fiber server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/linuxfight/yandexCalcApi/internal/services/logger"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	localTime := time.Now()
+	timezone, _ := localTime.Zone()
+
+	app := fiber.New()
+	logger.New(
+		true,
+		timezone,
+	)
+
+	logger.Log.Debug("logger initialized")
+	logger.Log.Debugf("current timezone: %s", timezone)
+
+	app.Post("/api/v1/calculate", func(c fiber.Ctx) error {
+
+		return c.JSON(fiber.Map{
+			"Status": "ok",
+		})
+	})
+
+	err := app.Listen(":8080")
+	if err != nil {
+		logger.Log.Panicf("listening error: %s", err.Error())
+		return
+	}
 }
